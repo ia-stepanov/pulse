7c1a7205ed104156b0c334e27bec60bcc1775ff9
Igor Stepanov <igorstepanov@hotmail.com>
2022-09-11 22:47:37 +0300

    build(Pulse): сверстать и адаптировать блоки: Каталог пульсометров и Отзывы клиентов; перепроектировать интерактивность

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,51 @@
 $(document).ready(function () {
-  $(".carousel__inner").slick({
+  $('.carousel__inner').slick({
+    dots: false,
+    infinite: true,
     speed: 1200,
-    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    autoplay: true,
+    autoplaySpeed: 5000,
     responsive: [
       {
-        breakpoint: 992,
+        breakpoint: 910,
         settings: {
           dots: true,
+          dotsClass: 'slick-dots',
           arrows: false,
         },
       },
+
     ],
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active')
+      .siblings()
+      .removeClass('catalog__tab_active')
+      .closest('div.container')
+      .find('div.catalog__content')
+      .removeClass('catalog__content_active')
+      .eq($(this).index())
+      .addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
